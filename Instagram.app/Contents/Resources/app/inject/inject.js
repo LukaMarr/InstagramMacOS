ddd4c214b10310b3e5c8e9d7efa4ea6be21628ec
Luka Marr <lumarcec@unin.hr>
2017-05-21 18:59:30 +0200

    app

---
+++ Instagram.app/Contents/Resources/app/inject/inject.js
@@ -0,0 +1,16 @@
+
+
+const {BrowserWindow} = require('electron')
+let win = new BrowserWindow({transparent: true, frame: false})
+win.show()
+
+//window.styleMask.insert(NSFullSizeContentViewWindowMask)
+
+//const {BrowserWindow} = require('electron')
+//let win = new BrowserWindow({titleBarStyle: 'hidden'})
+//win.show()
+
+//window.titlebarAppearsTransparent = true
+//window.titleVisibility = .Hidden
+//window.styleMask |= NSFullSizeContentViewWindowMask 
+
